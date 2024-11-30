7ece9a371fa7ca25d9cb4221f1f0d36f6f1a3fbb
Johan Reyes <152338154+AReyes19000@users.noreply.github.com>
2024-11-30 15:11:40 -0600

    script.js

--- registrarse/script.js
+++ registrarse/script.js
@@ -1,18 +1,22 @@
-let singUp = document.getElementById("singUp");
-let singIn = document.getElementById("singIn");
-let nameInput = document.getElementById("nameInput");
-let title = document.getElementById("title");
+const nombre = document.getElementById("name")
+const email = document.getElementById("email")
+const pas = document.getElementById("password")
+const text = document.getElementById("message")
+const form = document.getElementById("form")
 
-singIn.onclick = function () {
-  nameInput.style.maxHeight = "0";
-  title.innerHTML = "Login";
-  singUp.classList.add("disable");
-  singIn.classList.remove("disable");
-};
-
-singUp.onclick = function () {
-  nameInput.style.maxHeight = "60px";
-  title.innerHTML = "Registrarse";
-  singUp.classList.remove("disable");
-  singIn.classList.add("disable");
-};
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let regexEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,4})+$/;
+    if(nombre.value.length <6){
+        alert("El nombre no es valido");
+    }
+    if(!regexEmail.test(email.value)){
+        alert("El email no es valido");
+    }
+    if(pas.value.length <8){
+        alert("La contraseña no es valida, debe ser mayor a 8 caracteres");
+    }
+    if(text.value.length >=20){
+        alert("El mensaje no es valido, debe ser menor o igual a 20 caracteres")
+    }
+})
